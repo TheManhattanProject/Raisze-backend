7c708b1d1ba0338fc8e743d28a0293930808d376
Sarthak Bansal <sbansal793@gmail.com>
2022-04-07 01:24:34 +0530

    started create campaign api

---
+++ campaign_statistics/serializers.py
@@ -0,0 +1,20 @@
+from rest_framework import serializers
+from django.contrib.auth import get_user_model
+from .models import Campaign
+
+
+
+class CreateCampaignSerializer(serializers.ModelSerializer):
+
+
+    class Meta:
+        model = Campaign
+        fields = '__all__'
+
+    # def create(self, validated_data):
+    #     creator=get_user_model().objects.get(email=validated_data["creatorEmail"])
+    #     validated_data.pop("creatorEmail")
+    #     trip=Trips(creator=creator,**validated_data)
+    #     trip.save()
+    #     TripRating.objects.create(trip=trip)
+    #     return trip
--- campaign_statistics/views.py
+++ campaign_statistics/views.py
@@ -1,4 +1,18 @@
 from django.shortcuts import render
-# Create your views here.
+from rest_framework import generics,status
+from rest_framework.response import Response
+from .models import Campaign
+from .serializers import CreateCampaignSerializer
 
 
+class CreateTripView(generics.CreateAPIView):
+    serializer_class = CreateCampaignSerializer
+    queryset=Campaign.objects.all()
+
+    def create(self, request, *args, **kwargs):
+        # The request should be made in json format with POST
+        serializer = self.get_serializer(data={**request.data,"creatorEmail":request.user.email})
+        serializer.is_valid(raise_exception=True)
+        self.perform_create(serializer)
+        headers = self.get_success_headers(serializer.data)
+        return Response(serializer.data, status=status.HTTP_201_CREATED, headers=headers)
