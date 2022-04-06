79e9b617314c5ef814cbd8d89fa77104270ccb23
Sarthak Bansal <sbansal793@gmail.com>
2022-04-07 01:14:00 +0530

    added admin

--- campaign_statistics/admin.py
+++ campaign_statistics/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
-
-# Register your models here.
+from .models import Category,SubCategory,Country,Gender,Tags,Campaign,Items,Reward
+admin.site.register(Category)
+admin.site.register(SubCategory)
+admin.site.register(Country)
+admin.site.register(Gender)
+admin.site.register(Tags)
+admin.site.register(Campaign)
+admin.site.register(Items)
+admin.site.register(Reward)
