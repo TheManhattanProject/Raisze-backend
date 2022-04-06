c2597ef01cfd0421d61e014e4a6ab1d79f1eb4d9
Sarthak Bansal <sbansal793@gmail.com>
2022-04-07 01:10:01 +0530

    added user APIs

--- api/urls.py
+++ api/urls.py
@@ -1,8 +1,14 @@
 from .views import *
 from django.urls import path, include
 
+
+
+
+
 urlpatterns = [
     # path('rest-auth/facebook/', FacebookLogin.as_view(), name='fb_login'),
+    path('', include('dj_rest_auth.urls')),
+    path('registration/', include('dj_rest_auth.registration.urls')),
     path('rest-auth/google/', GoogleLogin.as_view(), name='google_login')
 
 ]
