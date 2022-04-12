a09a799fb9255976e5fc82ebb4ea6af1c0eb786c
pranav2305 <pranav2305@gmail.com>
2022-04-12 12:50:48 +0530

    Fix errors

--- api/urls.py
+++ api/urls.py
@@ -1,6 +1,6 @@
 from .views import *
 from django.urls import path, include
-
+from articles.views import *
 
 
 
@@ -9,6 +9,7 @@ urlpatterns = [
     # path('rest-auth/facebook/', FacebookLogin.as_view(), name='fb_login'),
     path('', include('dj_rest_auth.urls')),
     path('registration/', include('dj_rest_auth.registration.urls')),
-    path('rest-auth/google/', GoogleLogin.as_view(), name='google_login')
-
+    path('rest-auth/google/', GoogleLogin.as_view(), name='google_login'),
+    path('create_article/', CreatePostView.as_view(), name='create_article'),
+    path('articles/', ListArticleView.as_view(), name='articles'),
 ]
--- api/views.py
+++ api/views.py
@@ -7,30 +7,32 @@ from django.contrib.auth import get_user_model
 #     adapter_class = FacebookOAuth2Adapter
 
     
-# class GoogleLogin(GoogleOAuth2Adapter):
-#     adapter_class = GoogleOAuth2Adapter()
-#     serializer_class = GoogleOAuth2Adapter.get_provider().get_serializer_class()
-#     def get_callback_url(self, request):
-#     # def get_callback_url(self, request)
+class GoogleLogin(SocialLoginView):
+    adapter_class = GoogleOAuth2Adapter
+    callback_url = 'http://localhost:3000'
+    # adapter_class = GoogleOAuth2Adapter()
+    # serializer_class = GoogleOAuth2Adapter.get_provider().get_serializer_class()
+    # def get_callback_url(self, request):
+    # # def get_callback_url(self, request)
 
-#     def login(self, request, *args, **kwargs):
-#         print(request)
-#         # user,_ = get_user_model().objects.get_or_create(email=request.data['email'])
-#         # print(user)
-#         return super(GoogleLogin, self).login(request, *args, **kwargs)
+    # def login(self, request, *args, **kwargs):
+    #     print(request)
+    #     # user,_ = get_user_model().objects.get_or_create(email=request.data['email'])
+    #     # print(user)
+    #     return super(GoogleLogin, self).login(request, *args, **kwargs)
 
-from google.oauth2 import id_token
-from google.auth.transport import requests
+# from google.oauth2 import id_token
+# from google.auth.transport import requests
 
-from allauth.socialaccount.providers.oauth2.views import (
-    OAuth2Adapter,
-    OAuth2CallbackView,
-    OAuth2LoginView,
-)
-from allauth.socialaccount.providers.google.views import GoogleOAuth2Adapter
+# from allauth.socialaccount.providers.oauth2.views import (
+#     OAuth2Adapter,
+#     OAuth2CallbackView,
+#     OAuth2LoginView,
+# )
+# from allauth.socialaccount.providers.google.views import GoogleOAuth2Adapter
 
 
-class GoogleLogin(PublicApiMixin, ApiErrorsMixin, APIView):
+# class GoogleLogin(PublicApiMixin, ApiErrorsMixin, APIView):
     
 
 
