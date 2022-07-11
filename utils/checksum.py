2e1f326753309e3a4b50e0a16f03df8e6a20a764
Sidd438 <f20210438@pilani.bits-pilani.ac.in>
2022-07-11 13:11:43 +0530

    checksum

--- utils/checksum.py
+++ utils/checksum.py
@@ -1,12 +1,12 @@
 import paytmchecksum
 from django.conf import settings
 
-data = {'ORDERID': 'PAY2ME20220711ODR17', 'MID': 'waJxHx32224764812860', 'TXNID': '20220711111212800110168564903891272',
+def checksum(data = {'ORDERID': 'PAY2ME20220711ODR17', 'MID': 'waJxHx32224764812860', 'TXNID': '20220711111212800110168564903891272',
         'TXNAMOUNT': '122.00', 'PAYMENTMODE': 'CC', 'CURRENCY': 'INR', 'TXNDATE': '2022-07-11 12:39:55.0', 'STATUS': 'TXN_SUCCESS',
         'RESPCODE': '01', 'RESPMSG': 'Txn Success', 'GATEWAYNAME': 'HDFC', 'BANKTXNID': '777001965037051',
         'BANKNAME': 'JPMorgan Chase Bank',
-        'CHECKSUMHASH': 'gztRh8UjxpcvaeAlfBpM0cZU3rLTIyjw8YT0a6bwnV/TgSrx47W7seF6ZQAnLFqtswIEGueVa9b1VT7dWcNT8tLz3r2haBkuIujAKC0vJfI='}
+        'CHECKSUMHASH': 'gztRh8UjxpcvaeAlfBpM0cZU3rLTIyjw8YT0a6bwnV/TgSrx47W7seF6ZQAnLFqtswIEGueVa9b1VT7dWcNT8tLz3r2haBkuIujAKC0vJfI='}):
 
 
-print(paytmchecksum.verifySignature(
-    data, settings.PAYTM_SECRET_KEY, data['CHECKSUMHASH']))
+    print(paytmchecksum.verifySignature(
+        data, settings.PAYTM_SECRET_KEY, data['CHECKSUMHASH']))
